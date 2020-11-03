5ec6122c2149b47604d094a679fb4ace10ed4dd8
rolandpinter <pinter.roland.ms@gmail.com>
2020-11-03 20:28:08 +0100

    WORKING reduction version: eliminated lame if statements, made it N independent in a way that now it can handle any N value hence it is a for loop drive kernel launcher

--- project_mean_var/mean_var.cpp
+++ project_mean_var/mean_var.cpp
@@ -21,7 +21,7 @@ int main()
         program.build({ device });
 
         // Create a dummy testing vector
-        size_t N_original = 1000; // N should be between 2 - 2^24 for the project
+        size_t N_original = 2500; // N should be between 2 - 2^24 = 16777216 for the project
         size_t N = N_original;    // will be overwritten at each kernel launches
         std::vector<float> data(N);
         for (int i = 0; i < N; ++i)
@@ -37,7 +37,7 @@ int main()
         if (N % workGroupSize != 0)
         {
             data.resize(N + (workGroupSize - (N % workGroupSize)), 0.0);
-            std::cout << "Resize was needed, from N = " << N << " to N = " << data.size() << " (appended elements are zeros)!" <<std::endl;
+            std::cout << "starting data: resize was needed, from N = " << N << " to N = " << data.size() << " (appended elements are zeros)!" <<std::endl;
             N = data.size();
         }
         
@@ -58,152 +58,75 @@ int main()
         std::cout << "Number of required kernel launches = "<< n_launch <<" (for N = " << N << ", work group size = " << workGroupSize << ")"<< std::endl;
 
         // Vectors for storing the results for each kernel launched
-        std::vector<float> result1, result2, result3;
-        
-        // The first kernel launch will always be needed
-        if(true)
+        std::vector< std::vector<float> > results(n_launch);
+
+        for(int ilaunch = 0; ilaunch < n_launch; ++ilaunch)
         {
-            // Number of work groups
-            auto numWorkGroups = data.size() / workGroupSize;
-            
-            // Result vector holding the first kernel launch partial sums
-            result1.resize(numWorkGroups);
+            int numWorkGroups;
 
-            // Create buffers
-            cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * data.size(), data.data());
-            cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
-
-            // reduction kernel takes 3 arguments
-            kernel.setArg(0, buf_in);                                 //__global float* data
-            kernel.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
-            kernel.setArg(2, buf_out);                                //__global float* result
-
-
-            // Start kernel and read the buf_out
-            queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
-            queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, result1.data());
-        }
-
-        std::cout<<"result1 vector"<<std::endl;
-        for(int i=0;i<result1.size();++i)
-            std::cout<<result1[i]<<std::endl;
-
-        // Second kernel launch
-        if(result1.size() > 1)
-        {
-            // Check if N % workGroupSize is 0, if not, append 0s to data
-            N = result1.size();
-            if (N % workGroupSize != 0)
+            // First kernel launch always needed
+            if (ilaunch == 0)
             {
-                result1.resize(N + (workGroupSize - (N % workGroupSize)), 0.0);
-                std::cout << "Resize was needed, from N = " << N << " to N = " << result1.size() << " (appended elements are zeros)!" <<std::endl;
-                N = result1.size();
+                numWorkGroups = data.size() / workGroupSize;
+                
+                // Result vector holding the first kernel launch partial sums
+                results[ilaunch].resize(numWorkGroups);
+
+                // Create buffers
+                cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * data.size(), data.data());
+                cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
+
+                // reduction kernel takes 3 arguments
+                kernel.setArg(0, buf_in);                                 //__global float* data
+                kernel.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
+                kernel.setArg(2, buf_out);                                //__global float* result
+
+
+                // Start kernel and read the buf_out
+                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
+                queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, results[ilaunch].data());
             }
 
-            // Number of work groups
-            auto numWorkGroups = result1.size() / workGroupSize;
-            
-            // Result vector holding the second kernel launch partial sums
-            result2.resize(numWorkGroups);
-
-            // Create buffers
-            cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * result1.size(), result1.data());
-            cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
-
-            // reduction kernel takes 3 arguments
-            kernel.setArg(0, buf_in);                                 //__global float* data
-            kernel.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
-            kernel.setArg(2, buf_out);                                //__global float* result
-
-
-            // Start kernel and read the buf_out
-            queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
-            queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, result2.data());
-        }
-
-        std::cout<<"result2 vector"<<std::endl;
-        for(int i=0;i<result2.size();++i)
-            std::cout<<result2[i]<<std::endl;
-
-        // Third kernel launch
-        if(result2.size() > 1)
-        {
-            // Check if N % workGroupSize is 0, if not, append 0s to data
-            N = result2.size();
-            if (N % workGroupSize != 0)
+            // If more kernel launches are needed
+            else
             {
-                result2.resize(N + (workGroupSize - (N % workGroupSize)), 0.0);
-                std::cout << "Resize was needed, from N = " << N << " to N = " << result2.size() << " (appended elements are zeros)!" <<std::endl;
-                N = result2.size();
+                // Check if N % workGroupSize is 0, if not, append 0s to data
+                N = results[ilaunch - 1].size();
+                if (N % workGroupSize != 0)
+                {
+                    results[ilaunch - 1].resize(N + (workGroupSize - (N % workGroupSize)), 0.0);
+                    std::cout << "ilaunch" <<ilaunch << ": resize was needed, from N = " << N << " to N = " << results[ilaunch - 1].size() << " (appended elements are zeros)!" <<std::endl;
+                    N = results[ilaunch - 1].size();
+                }
+                // Number of work groups
+                numWorkGroups = results[ilaunch - 1].size() / workGroupSize;
+
+                // Result vector holding the (ilaunch+1)th kernel launch partial sums
+                results[ilaunch].resize(numWorkGroups);
+
+                // Create buffers
+                cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * results[ilaunch - 1].size(), results[ilaunch - 1].data());
+                cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
+
+                // reduction kernel takes 3 arguments
+                kernel.setArg(0, buf_in);                                 //__global float* data
+                kernel.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
+                kernel.setArg(2, buf_out);                                //__global float* result
+
+
+                // Start kernel and read the buf_out
+                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
+                queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, results[ilaunch].data());
+
             }
-
-            // Number of work groups
-            auto numWorkGroups = result2.size() / workGroupSize;
             
-            // Result vector holding the third kernel launch partial sums
-            result3.resize(numWorkGroups);
-
-            // Create buffers
-            cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * result2.size(), result2.data());
-            cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
-
-            // reduction kernel takes 3 arguments
-            kernel.setArg(0, buf_in);                                 //__global float* data
-            kernel.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
-            kernel.setArg(2, buf_out);                                //__global float* result
-
-
-            // Start kernel and read the buf_out
-            queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
-            queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, result3.data());
         }
 
-        std::cout<<"result3 vector"<<std::endl;
-        for(int i=0;i<result3.size();++i)
-            std::cout<<result3[i]<<std::endl;
+        // Check if final result vector indeed stores 1 final value
+        std::cout << "number of elements in last result vector = " << results[n_launch - 1].size() << std::endl;
+        std::cout << "Final result is: " << results[n_launch - 1][0] << std::endl;
+
        
-
-        
-
-
-
-        /*
-        // Check if N % workGroupSize is 0, if not, append 0s to data
-        if (N % workGroupSize != 0)
-        {
-            data.resize(N + (workGroupSize - (N % workGroupSize)), 0.0);
-            std::cout << "Resize was needed, from N = " << N << " to N = " << data.size() << " (appended elements are zeros)!" <<std::endl;
-            N = data.size();
-        }
-
-        // Number of work groups
-        auto numWorkGroups = data.size() / workGroupSize;
-        
-        // Result vector holding the partial sums
-        std::vector<float> result(numWorkGroups);
-
-        // Create buffers
-        cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * data.size(), data.data());
-        cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
-
-        // reduction kernel takes 3 arguments
-        kernel.setArg(0, buf_in);                                 //__global float* data
-        kernel.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
-        kernel.setArg(2, buf_out);                                //__global float* result
-
-
-        // Start kernel and read the buf_out
-        queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
-        queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, result.data());
-
-        // Print stuff
-        std::cout << "data size (N) = " << N << std::endl;
-        std::cout << "work group size = " << workGroupSize << std::endl;
-        std::cout << "number of work groups = " << numWorkGroups << std::endl;
-        std::cout << "Results: " << std::endl;
-        for(int i = 0; i < numWorkGroups; ++i)
-            std::cout << result[i] << std::endl;
-        */
     }/// end of try case
     
     catch (cl::BuildError& error) // If kernel failed to build
