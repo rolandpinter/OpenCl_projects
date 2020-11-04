ddb353f17fa567b7ea4126b3864b02298b5fb03d
rolandpinter <pinter.roland.ms@gmail.com>
2020-11-04 12:54:40 +0100

    added a mean calculator kernel, so all of the computations are done via GPU

---
+++ project_mean_var/mean.cl
@@ -0,0 +1,31 @@
+__kernel void mean(__global float* data, __local float* localData, __global float* result, int N_original_dataset) 
+{
+    int gid = get_global_id(0);        // id of the work item amongs every work items 
+    int lid = get_local_id(0);         // id of the work item in the workgroup
+    int localSize = get_local_size(0); // size of the workgroup
+
+    // transfer from global to local memory
+    localData[lid] = data[gid];
+
+    // make sure everything up to this point in the workgroup finished executing
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    // perform reduction in local memory
+    for(unsigned int s = 1; s < localSize; s *= 2)
+    {
+        // sum every adjacent pairs
+        if (lid % (2 * s) == 0)
+        {
+            localData[lid] += localData[lid + s];
+        }
+
+        // make sure everything is synchronized properly before we go into the next iteration
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+     if(lid == 0)
+     {
+         // id of the workgroup = get_group_id()
+        result[get_group_id(0)] =  localData[0] / N_original_dataset;
+     }
+}
--- project_mean_var/mean_var.cpp
+++ project_mean_var/mean_var.cpp
@@ -10,25 +10,32 @@ int main()
         cl::Context context = queue.getInfo<CL_QUEUE_CONTEXT>();
         cl::Platform platform{device.getInfo<CL_DEVICE_PLATFORM>()};
 
-        // Load reduction.cl kernel source file
+        // Load kernel source files
         std::ifstream source_file{ "../reduction.cl" };
+        std::ifstream source_file_mean{ "../mean.cl" };
         if (!source_file.is_open())
             throw std::runtime_error{ std::string{ "Cannot open kernel source: " } + "reduction.cl" };
+        if (!source_file_mean.is_open())
+            throw std::runtime_error{ std::string{ "Cannot open kernel source: " } + "mean.cl" };
     
-        // Create cl::Program from kernel and build it for the device
+        // Create cl::Program from kernels and build them for the device
         cl::Program program{ std::string{ std::istreambuf_iterator<char>{ source_file },
                                           std::istreambuf_iterator<char>{} } };
+        cl::Program program_mean{ std::string{ std::istreambuf_iterator<char>{ source_file_mean },
+                                          std::istreambuf_iterator<char>{} } };
         program.build({ device });
+        program_mean.build({ device });
 
         // Create a dummy testing vector
-        size_t N_original = 16777216; // N should be between 2 - 2^24 = 16777216 for the project
+        size_t N_original = 1024; // N should be between 2 - 2^24 = 16777216 for the project
         size_t N = N_original;    // will be overwritten at each kernel launches
         std::vector<float> data(N);
         for (int i = 0; i < N; ++i)
             data[i] = 1.0;
         
-         // Create kernel from program
+         // Create kernel from programs
         cl::Kernel kernel(program, "reduction");
+        cl::Kernel kernel_mean(program_mean, "mean");
 
         // Access work group size 
         auto workGroupSize = kernel.getWorkGroupInfo<CL_KERNEL_WORK_GROUP_SIZE>(device);
@@ -87,8 +94,8 @@ int main()
                 queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, results[ilaunch].data());
             }
 
-            // If more kernel launches are needed
-            else
+            // If more kernel launches are needed but not the last kernel launch
+            else if ((ilaunch > 0) && (ilaunch < (n_launch - 1)))
             {
                 // Check if N % workGroupSize is 0, if not, append 0s to data
                 N = results[ilaunch - 1].size();
@@ -119,18 +126,46 @@ int main()
                 queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, results[ilaunch].data());
 
             }
+
+            // Last kernel launch
+            else if (ilaunch == (n_launch - 1))
+            {
+                // Check if N % workGroupSize is 0, if not, append 0s to data
+                N = results[ilaunch - 1].size();
+                if (N % workGroupSize != 0)
+                {
+                    results[ilaunch - 1].resize(N + (workGroupSize - (N % workGroupSize)), 0.0);
+                    std::cout << "ilaunch" <<ilaunch << ": resize was needed, from N = " << N << " to N = " << results[ilaunch - 1].size() << " (appended elements are zeros)!" <<std::endl;
+                    N = results[ilaunch - 1].size();
+                }
+                // Number of work groups
+                numWorkGroups = results[ilaunch - 1].size() / workGroupSize;
+
+                // Result vector holding the (ilaunch+1)th kernel launch partial sums
+                results[ilaunch].resize(numWorkGroups);
+
+                // Create buffers
+                cl::Buffer buf_in(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(float) * results[ilaunch - 1].size(), results[ilaunch - 1].data());
+                cl::Buffer buf_out(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(float) * numWorkGroups);
+
+                // reduction kernel takes 3 arguments
+                kernel_mean.setArg(0, buf_in);                                 //__global float* data
+                kernel_mean.setArg(1, sizeof(float) * workGroupSize, nullptr); //__local float* localData
+                kernel_mean.setArg(2, buf_out);                                //__global float* result
+                kernel_mean.setArg(3, N_original);                             //int N_original_dataset
+
+
+                // Start kernel and read the buf_out
+                queue.enqueueNDRangeKernel(kernel_mean, cl::NullRange, cl::NDRange(N), cl::NDRange(workGroupSize));
+                queue.enqueueReadBuffer(buf_out, true, 0, sizeof(float) * numWorkGroups, results[ilaunch].data());
+
+            }
             
         }
 
-        // Check if final result vector indeed stores 1 final value
         std::cout << "number of elements in last result vector  = " << results[n_launch - 1].size() << std::endl;
-        std::cout << "Final sum is: " << results[n_launch - 1][0] << std::endl;
-
-        // Dividing the sum of the input data with the original data size <- determining the mean of the dataset
-        float mean_of_data = results[n_launch - 1][0] / N_original;
-
         std::cout << "\n###############################" << std::endl;
-        std::cout << "Mean of the input data = " << mean_of_data << std::endl;
+        std::cout << "Mean of the input data = " << results[n_launch - 1][0] << std::endl;
         std::cout << "###############################" << std::endl;
 
        
